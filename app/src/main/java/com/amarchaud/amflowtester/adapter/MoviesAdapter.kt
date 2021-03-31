f6a10faa409c435418ac6ea74f955d7f4524f5b0
Antoine Marchaud <tonio2835@gmail.com>
2021-03-31 13:42:56 +0200

    clean adapter

--- app/src/main/java/com/amarchaud/amflowtester/adapter/MoviesAdapter.kt
+++ app/src/main/java/com/amarchaud/amflowtester/adapter/MoviesAdapter.kt
@@ -4,16 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable
-import com.amarchaud.amflowtester.R
 import com.amarchaud.amflowtester.application.Config
 import com.amarchaud.amflowtester.databinding.ItemListingBinding
 import com.amarchaud.amflowtester.interfaces.IMovieClickListener
 import com.amarchaud.amflowtester.model.entity.MovieEntity
 import com.amarchaud.amflowtester.utils.GenreUtils
 import com.amarchaud.amflowtester.utils.GlideUtils
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 
 class MoviesAdapter(
     private val onClickListener: IMovieClickListener
@@ -28,23 +24,19 @@ class MoviesAdapter(
         return ItemListingViewHolder(binding)
     }
 
-
     inner class ItemListingViewHolder(var binding: ItemListingBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: MovieEntity) = with(binding) {
 
+            tvTitle.text = item.title
+            tvGenre.text = GenreUtils.getGenre(item.genre_ids)
+
+            GlideUtils.createGlide(itemView.context, Config.IMAGE_URL + item.poster_path, ivPoster)
 
             itemView.setOnClickListener {
                 onClickListener.onMovieClicked(item)
             }
-
-
-            tvTitle.text = item.title
-
-            GlideUtils.createGlide(itemView.context, Config.IMAGE_URL + item.poster_path, ivPoster)
-
-            tvGenre.text = GenreUtils.getGenre(item.genre_ids)
         }
     }
 
@@ -53,8 +45,4 @@ class MoviesAdapter(
         val item = getItem(position)
         item?.let { holder.bind(it) }
     }
-}
-
-data class User(val s: String, val i: Int) {
-
 }
